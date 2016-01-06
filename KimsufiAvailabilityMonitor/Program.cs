e4be22561a3ab72c9e22c3a9f62c925e59a266cc
Albireo <kappa7194@hotmail.it>
2016-01-06 21:05:48 +0100

    Create console application project.

---
+++ KimsufiAvailabilityMonitor/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace KimsufiAvailabilityMonitor
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ KimsufiAvailabilityMonitor/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KimsufiAvailabilityMonitor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Albireo")]
+[assembly: AssemblyProduct("KimsufiAvailabilityMonitor")]
+[assembly: AssemblyCopyright("Copyright © Albireo 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4a12abdf-f15f-477d-bc2c-ad16b48b71a2")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
